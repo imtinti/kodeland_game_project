fa422de43658dfc8df8b55e4728e15f1fba5d186
iuri <47192296+imtinti@users.noreply.github.com>
2024-07-26 11:17:06 -0300

    features: handle user controls
    - enter to pause
    - - esc to back
    - - m to go back main menu (reset game)

--- kodland_simple_basic_game.py
+++ kodland_simple_basic_game.py
@@ -29,6 +29,7 @@ game_state = MAIN_MENU
 # State captions options
 HOME_SCREEN = 'HOME'
 GAME_SCREEN = 'GAME'
+PAUSE_SCREEN = 'PAUSE'
 
 # Screen dimensions
 WIDTH, HEIGHT = 800, 600
@@ -48,6 +49,10 @@ def main_menu():
     draw_text('Main Menu', font, BLACK, screen, 20, 20)
     draw_text('Press ENTER to Start', small_font, BLACK, screen, 20, 100)
     draw_text('Press Q to Quit', small_font, BLACK, screen, 20, 140)
+
+    # Setting caption screen
+    pygame.display.set_caption(HOME_SCREEN)
+
     pygame.display.update()
 
 def pause_menu():
@@ -55,9 +60,13 @@ def pause_menu():
     draw_text('Pause Menu', font, BLACK, screen, 20, 20)
     draw_text('Press ESC to Resume', small_font, BLACK, screen, 20, 100)
     draw_text('Press M to Main Menu', small_font, BLACK, screen, 20, 140)
+
+    # Setting caption screen
+    pygame.display.set_caption(PAUSE_SCREEN)
+
     pygame.display.update()
 
-def game_loop():
+def game_loop(player, enemies):
     # Screen dimensions
     WIDTH_GAME, HEIGHT_GAME = 600, 700
     screen = pygame.display.set_mode((WIDTH_GAME, HEIGHT_GAME))
@@ -65,10 +74,8 @@ def game_loop():
     # Setting caption screen
     pygame.display.set_caption(GAME_SCREEN)
 
-    player = pygame.Rect(50, 50, 50, 50)
-    enemies = [pygame.Rect(200, 150, 50, 50), pygame.Rect(400, 300, 50, 50)]
-    
     while game_state == GAME_RUNNING:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -88,43 +95,76 @@ def game_loop():
             player.y += 5
 
         screen.fill(WHITE)
+
         pygame.draw.rect(screen, RED, player)
+        
         for enemy in enemies:
             pygame.draw.rect(screen, BLACK, enemy)
 
         # Draw text on the game screen
-        draw_text('Score: 0', small_font, BLACK, screen, 20, 20)
+        draw_text('Press ENTER to PAUSE', small_font, BLACK, screen, 20, 20)
+        draw_text('Use arrows to control the red block', small_font, BLACK, screen, 20, 40)
+
 
         pygame.display.update()
         clock.tick(60)
 
     return MAIN_MENU
 
-while True:
-    if game_state == MAIN_MENU:
-        main_menu()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    game_state = GAME_RUNNING
-                if event.key == pygame.K_q:
+def run_game():
+
+    global game_state
+
+    default_player_position_init =[50, 50, 50, 50]
+    # Initialize player and enemies outside of game_loop to keep their state
+    player = pygame.Rect(default_player_position_init)
+
+    default_enemie_position_init_one = [200, 150, 50, 50]
+    default_enemie_position_init_two = [400, 300, 50, 50]
+
+    enemies = [pygame.Rect(default_enemie_position_init_one), pygame.Rect(default_enemie_position_init_two)]
+
+    while True:
+
+        #MAIN MENU
+        if game_state == MAIN_MENU:
+            main_menu()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        game_state = GAME_RUNNING
+                    if event.key == pygame.K_q:
+                        pygame.quit()
+                        sys.exit()
+
+        #GAME RUNNING
+        elif game_state == GAME_RUNNING:
+            game_state = game_loop(player, enemies)
+
+        #GAME PAUSED
+        elif game_state == PAUSE_MENU:
+            pause_menu()
+            for event in pygame.event.get():
+
+                #handle button quit click event
+                if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-    elif game_state == GAME_RUNNING:
-        game_state = game_loop()
+                #handle enter keydown
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        game_state = GAME_RUNNING
 
-    elif game_state == PAUSE_MENU:
-        pause_menu()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    game_state = GAME_RUNNING
-                if event.key == pygame.K_m:
-                    game_state = MAIN_MENU
+                    if event.key == pygame.K_m:
+                        #reset player positions
+                        enemies = [pygame.Rect(default_enemie_position_init_one), pygame.Rect(default_enemie_position_init_two)]
+                        player = pygame.Rect(default_player_position_init)
+                        
+                        #goback to main menu
+                        game_state = MAIN_MENU
+
+run_game()
